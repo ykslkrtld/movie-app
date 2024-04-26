827e3b61ef5f5afa6963e4a9a41665f4dc62a803
yksl <yukselkurtuldu9@gmail.com>
2024-04-26 15:40:40 +0300

    edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -84,7 +84,7 @@ export default function Navbar() {
                       {({ active }) => (
                         <span
                           role="button"
-                          onClick={() => logOut()}
+                          onClick={logOut}
                           className={classNames(
                             active ? "bg-gray-100" : "",
                             "block px-4 py-2 text-sm text-gray-700 cursor-pointer"
